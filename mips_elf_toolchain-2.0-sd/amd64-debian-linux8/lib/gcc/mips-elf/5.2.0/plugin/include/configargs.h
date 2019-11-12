a9ca589b7aa64dfad83c8407962b901e57bfe88b
root <root@localhost.localdomain>
2019-11-12 21:26:27 +0000

    Files

---
+++ mips_elf_toolchain-2.0-sd/amd64-debian-linux8/lib/gcc/mips-elf/5.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/scratch/build/mips_elf_toolchain-2.0-sd//gcc-5.2.0/configure -v --prefix=/usr/pack/mips_elf_toolchain-2.0-sd --target=mips-elf --enable-languages=c --with-newlib --disable-shared --disable-nls --disable-libssp --exec-prefix=/usr/pack/mips_elf_toolchain-2.0-sd/amd64-debian-linux8";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
